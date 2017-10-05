65413b1233f82bb6cbdc13ef0ff625993bd73bc8
Dirk Wenzel <wenzel@cps-it.de>
2017-10-05 10:33:27 +0200

    [TASK] obsolete settings removed from default TypoScript. Documentation added.

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,12 +1,9 @@
 
 plugin.tx_persons {
     view {
-        templateRootPaths.0 = EXT:persons/Resources/Private/Templates/
-        templateRootPaths.1 = {$plugin.tx_persons.view.templateRootPath}
-        partialRootPaths.0 = EXT:persons/Resources/Private/Partials/
-        partialRootPaths.1 = {$plugin.tx_persons.view.partialRootPath}
-        layoutRootPaths.0 = EXT:persons/Resources/Private/Layouts/
-        layoutRootPaths.1 = {$plugin.tx_persons.view.layoutRootPath}
+        templateRootPaths.0 = {$plugin.tx_persons.view.templateRootPath}
+        partialRootPaths.0 =  {$plugin.tx_persons.view.partialRootPath}
+        layoutRootPaths.0 = {$plugin.tx_persons.view.layoutRootPath}
     }
     persistence {
         storagePid = {$plugin.tx_persons.persistence.storagePid}
@@ -61,35 +58,3 @@ personPage {
     settings < plugin.tx_persons.settings
 }
 
-plugin.tx_persons._CSS_DEFAULT_STYLE (
-        textarea.f3-form-error {
-                background-color:#FF9F9F;
-                border: 1px #FF0000 solid;
-        }
-
-        input.f3-form-error {
-                background-color:#FF9F9F;
-                border: 1px #FF0000 solid;
-        }
-
-        .{extension.cssClassName} table {
-                border-collapse:separate;
-                border-spacing:10px;
-        }
-
-        .{extension.cssClassName} table th {
-                font-weight:bold;
-        }
-
-        .{extension.cssClassName} table td {
-                vertical-align:top;
-        }
-
-        .typo3-messages .message-error {
-                color:red;
-        }
-
-        .typo3-messages .message-ok {
-                color:green;
-        }
-)
