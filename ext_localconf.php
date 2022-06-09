f8fc6b997fbb7c63486d7d08beaa5f426789a1b5
Benjamin <brannow@users.noreply.github.com>
2022-06-09 14:57:50 +0700

    remove unused Namespace from plugins

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function () {
         TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'CPSIT.Persons',
+            'Persons',
             'Persons',
             [
                 CPSIT\Persons\Controller\PersonController::class => 'list, show, showSelected,filter'
